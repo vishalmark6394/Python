95f2fc1eecc1d740ee6b506b7ec63135b691864e
Vishal Singh <42601088+vishalmark6394@users.noreply.github.com>
2020-03-06 13:49:46 +0530

    Add files via upload

---
+++ JavaFile.java
@@ -0,0 +1,8 @@
+import  java.lang.*;
+public class Car
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Python
-Python Codes Only
+Trying to learn the git.
